47d5a9d7f6db9b2e58903d8c4eac0d8d94b75b5e
thelucyclub <thelucyclub@gmail.com>
2015-05-01 09:58:11 -0400

    Update run.bat

--- proxy/run/run.bat
+++ proxy/run/run.bat
@@ -1,11 +1,13 @@
 @echo off
 echo Locating local proxy hosts...
+ping 192.168.0.1
 echo Found 0 proxy(s)
 echo ==============[Hosts]===============
 echo error.noHostsFound
 echo ====================================
 echo STARTED PROXY LOGGING
 echo [NOTICE] Currently proxy logging is not implented, so if this doesn't work, then just know you're not doing anything wrong.
+echo mcpetransfer\exit.li exit 1
 goto :loop
 
 :loop
