e6354eb96e135f99ebd4e33f6e912adad5e59547
TheLucyClub Github Team <thelucyclub@gmail.com>
2016-03-30 14:39:47 -0400

    Update run.bat

--- proxy/run/run.bat
+++ proxy/run/run.bat
@@ -1,10 +1,10 @@
 @echo off
 echo STARTING RAKLIB BINARIE SOCKETS
 title Loading RakLib Binaries...
-sleep 5
+ping 1.2.3.4 -n 4000>nul
 echo Starting MCPE-Transfer-1.0.98_devBuild#3352
 title Loading Main/Proxy-Lib
-sleep 10
+ping 1.2.3.4 -n 5000>nul
 title MCPE-Transfer-1.0.98_devBuild#3352
 echo [Proxy] Started
 echo [ProxyThread/Info] Locating local proxy hosts...
@@ -15,20 +15,20 @@ echo ==============[Hosts]===============
 echo error.noHostsFound
 echo ====================================
 echo [ProxyThread/Notice] STARTED PROXY LOGGING
-timeout /t 1
+ping 1.2.3.4 -n 3000>nul
 echo [ProxyThread/Warning] Currently proxy logging is not implented, so if this doesn't work, then just know you're not doing anything wrong.
 echo To keep from overwelming the CPU, the proxy will sleep for 20 seconds
-sleep 20
+ping 1.2.3.4 -n 20000>nul
 goto :loop
 echo [ProxyThread/Notice] Done. Now switching to log
 
 :loop
 echo [RakLib/Main] Locating more hosts in 10 seconds !
-timeout /t 10
-ping 127.0.0.1:19132
-ping 192.168.local.local:port
-ping 192.168.local.local:port
-ping 192.168.local.local:port
+ping 1.2.3.4 -n 2000>nul
+ping 127.0.0.1:19132>nul
+ping 192.168.local.local:port>nul
+ping 192.168.local.local:port>nul
+ping 192.168.local.local:port>nul
 /* You can also add external servers, e.g. ping sg.lbsg.net:19132 */
 echo [RakLib/Notice] %ping.network.resultsServers
 goto :loop
